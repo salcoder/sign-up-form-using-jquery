6d633a26970c7926387c173351d2071600fc5574
Paulo Oliva <coder@salcoder.com>
2014-12-04 18:54:16 -0500

    Project Finished

--- js/app.js
+++ js/app.js
@@ -1,62 +1,36 @@
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
+// Show hints based on password input information
+
+// Hide hints
+$("form p span").hide();
+
 var $password = $("#password");
 var $confirmPassword = $("#confirm_password");
 
-//Hide hints
-$("form span").hide();
-
-function isPasswordValid() {
-  return $password.val().length > 8;
-}
-
-function arePasswordsMatching() {
-  return $password.val() === $confirmPassword.val();
-}
-
-function canSubmit() {
-  return isPasswordValid() && arePasswordsMatching();
-}
-
-function passwordEvent(){
-    //Find out if password is valid  
-    if(isPasswordValid()) {
-      //Hide hint if valid
-      $password.next().hide();
-    } else {
-      //else show hint
-      $password.next().show();
-    }
+function passwordEvent() {
+  if($password.val().length > 8) {
+    // If valid, hide hint
+    $password.next().hide();
+  } else {
+    // else show hint  
+    $password.next().show();
+  }
 }
 
 function confirmPasswordEvent() {
-  //Find out if password and confirmation match
-  if(arePasswordsMatching()) {
-    //Hide hint if match
+  // Find out if password and confirmation match
+  if($password.val() === $confirmPassword.val()) {
+    // Hide hint if matched
     $confirmPassword.next().hide();
   } else {
-    //else show hint 
+    // Else, show hint
     $confirmPassword.next().show();
   }
 }
 
-function enableSubmitEvent() {
-  $("#submit").prop("disabled", !canSubmit());
-}
-
-//When event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-
-//When event happens on confirmation input
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-
-enableSubmitEvent();
-
-
-
-
-
-
-
-
-
+// See if password is valid
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+  
+  
+// When events happens on confirmation input
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+  
